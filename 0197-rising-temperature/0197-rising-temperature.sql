ab0b452d0f600e6d809e60aa705f23619b707ebf
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-18 15:48:37 +0900

    Time: 573 ms (60.31%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -2,3 +2,7 @@ select today.id
 from weather as today join weather as yesterday 
         on today.recordDate = date_add(yesterday.recordDate, Interval 1 day)
 where today.temperature > yesterday.temperature
+
+-- SELECT w1.id
+-- FROM Weather w1 JOIN Weather w2 ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+-- WHERE w1.temperature > w2.temperature;
