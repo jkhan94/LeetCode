e3962bd13ceeebe6332a57c6d4109a22ffde9fdd
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-01-18 15:51:56 +0900

    Time: 588 ms (8.65%), Space: 0B (100%) - LeetHub

--- 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -10,4 +10,23 @@ from (select *
     on s.machine_id=e.machine_id and s.process_id=e.process_id
 group by s.machine_id 
 
- 
+-- 모범답안1: 1.0을 곱하지 않으면 소수점 아래가 잘린다.
+-- SELECT 
+--     machine_id,
+--     ROUND(SUM(CASE 
+--                     WHEN activity_type='start' THEN timestamp*-1 
+--                         ELSE timestamp 
+--                 END)*1.0 / (SELECT COUNT(DISTINCT process_id)) ,3) 
+--         AS processing_time
+-- FROM Activity
+-- GROUP BY machine_id
+
+-- 모범답안2:
+-- SELECT a.machine_id, 
+--        ROUND(AVG(b.timestamp - a.timestamp), 3) AS processing_time
+-- FROM Activity a,  Activity b
+-- WHERE 
+--     a.machine_id = b.machine_id  AND    a.process_id = b.process_id 
+--     AND   
+--     a.activity_type = 'start'    AND    b.activity_type = 'end'
+-- GROUP BY machine_id
