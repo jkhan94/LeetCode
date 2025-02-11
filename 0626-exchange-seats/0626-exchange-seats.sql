f80327f386ef8cd688e1910561048fe139159a22
Jinkyung Han <163835909+jkhan94@users.noreply.github.com>
2025-02-12 01:05:40 +0900

    Time: 826 ms (16.68%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,6 +1,7 @@
 select case
-        when mod(id,2)=1 then if(id=(select max(id) from seat), id, id+1)
         when mod(id,2)=0 then id-1
+        when mod(id,2)=1 and id=(select max(id) from seat) then id        
+        else id+1
         end as id, student
 from seat
 order by id
